06619af5ed9c9496bbba7ea6805a061f8be321f2
ROHIT <kumarohit1202@gmail.com>
2024-12-31 13:04:59 +0530

    key secret

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false, // Disable source maps
-  }
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.NON_SENSITIVE': JSON.stringify(process.env.NON_SENSITIVE), // Exclude sensitive keys
+  },
 })
